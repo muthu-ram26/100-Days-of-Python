4e56eee752931195e5977a0809b9932b92f49751
luffy_taro26 <168455438+muthu-ram26@users.noreply.github.com>
2026-06-13 19:48:27 +0530

    Delete Days 2.py

--- Days 2.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator")
-bill=int(input("what is your bill\n"))
-tip = int(input("how much want give tip 10,12,15,20?\n"))
-people=int(input("how many people to split the bill?\n"))
-calculate=(tip+bill)/people
-print(f"Each person should pay :",calculate)
